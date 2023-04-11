72691c50a1594be4bd438a5a62c8ab04fb9e1599
Boterodario <dario.botero@hotmail.com>
2023-04-11 09:30:31 -0500

    Primer api con react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import imageRickmorty from './img/rick-morty.png';
+import './App.css';
+
+function App() {
+  const reqApi = async () =>{
+    const api = await fetch ("https://rickandmortyapi.com/api/character")
+    const characterApi = await api.json();
+    console.log(characterApi);
+
+
+    
+  };
+
+
+
+  return (
+    <div className='App'>
+    <header className='App-header'>
+    <h1 className='title'>Rick & Morty</h1>
+    <img src={imageRickmorty} alt='Rick & Morty' className='img-home' />
+    <button onClick={reqApi} className='btn-search'>Buscar Personajes</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
